40de5affb9a0e35b40ca8541f443ef8a9d17c83f
s.shimokawa <ssmjkopapplehonda@icloud.com>
2025-06-15 21:07:22 +0900

    db接続設定変更

--- backend/stockmanagerApp/settings.py
+++ backend/stockmanagerApp/settings.py
@@ -103,7 +103,7 @@ DATABASES = {
         "NAME": os.getenv("DB_NAME"),
         "USER": os.getenv("DB_USER"),
         "PASSWORD": os.getenv("DB_PASSWORD"),
-        "HOST": os.getenv("DB_HOST", "stockmanager-db.internal"),
+        "HOST": os.getenv("DB_HOST", "localhost"),
         "PORT": os.getenv("DB_PORT", "3306"),
     }
 }
